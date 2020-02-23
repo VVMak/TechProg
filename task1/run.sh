7189db281ca5738334ad864b7a2dcfc53d4391cf
makarovvladimir <makarov.vladimir@phystech.edu>
2020-02-23 20:04:22 +0300

    Task1

---
+++ task1/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+path=$1
+extension=$2
+directory=$3
+archive=$4
+cd ${path}
+mkdir -p ./${directory}
+filepath=$(find . -type f -name "*.$extension")
+for file in $filepath; do
+    folder=${file:1}
+    mkdir -p ./${directory}${folder%/*}
+    cp ${file} ./${directory}${file:1}
+done
+tar -cf ${archive} ./${directory}
+echo "done"
