6efb2e40ca07ffe1341df8898093ce90f5baabb8
makarovvladimir <makarov.vladimir@phystech.edu>
2020-02-24 15:46:16 +0300

    Исправлена директория сохранения папки и архива

--- task1/run.sh
+++ task1/run.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/usr/bin/bash
 path=$1
 extension=$2
 directory=$3
 archive=$4
-cd ${path}
 mkdir -p ./${directory}
-filepath=$(find . -type f -name "*.$extension")
+filepath=$(find ${path} -type f -name "*.${extension}")
 for file in $filepath; do
-    folder=${file:1}
-    mkdir -p ./${directory}${folder%/*}
-    cp ${file} ./${directory}${file:1}
+    folder=${file}
+    folder=${folder%/*}/
+    mkdir -p ./${directory}/${folder#${path}}
+    cp ${file} ./${directory}/${file#${path}}
 done
 tar -cf ${archive} ./${directory}
 echo "done"
